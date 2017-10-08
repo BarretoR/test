14a450c728efdd8097e7ba3f9d0e6e5d83e540af
chavesR <rogeriocbarreto@gmail.com>
2017-10-08 01:40:02 -0300

    Alteração nos arquivos main.m e Extract_faces.m

--- Extract_faces.m
+++ Extract_faces.m
@@ -1,10 +1,28 @@
 function Faces = Extract_faces(file_vid, file_eye_pos)
-    file_id = fopen(file_eye_pos);
-    eye_pos = textscan(file_id, '%d %d %d %d %d', 'Delimiter', ',');
+    fileID = fopen(file_eye_pos);
+    eye_pos = textscan(fileID, '%d %d %d %d %d', 'Delimiter', ',');
     vid = VideoReader(file_vid);
-
+    face_detector = vision.CascadeObjectDetector;
+    face_detector.MinSize = [450 450];
+    iframe = 0;
+    
     while hasFrame(vid)
+        iframe = iframe + 1;
         frame = readFrame(vid);
         
+        %Face Align
+        opposite = double(eye_pos{5}(iframe) - eye_pos{3}(iframe));
+        adjacent = double(eye_pos{4}(iframe) - eye_pos{2}(iframe));
+        angle_rotation = atand(opposite/adjacent);
+        frame = imrotate(frame, angle_rotation);
+        
+        temp = face_detector.step(frame);
+        if(~isempty(temp))
+            frame = imcrop(frame, temp);
+            Faces.data{iframe} = frame;
+            Faces.exist{iframe} = 1;
+        end
+        
     end
+fclose(fileID);
 end
--- main.m
+++ main.m
@@ -5,34 +5,15 @@ clc
 files_path_in = {'/mnt/rogerio/Mestrado/FaceSpoofing/Datasets/OULU_NPU/Train_files/'}
 files_path_out = {'/mnt/rogerio/Mestrado/FaceSpoofing/Datasets/OULU_NPU/Train_inputs/'}
 %%
-
+parpool;
 for i = 1:numel(files_path_in)
     files = dir([files_path_in{i},'*.avi'])
-    for j = 1:numel(files)
+    parfor j = 1:numel(files)
         j
         [pathstr, name, ext] = fileparts(files(j).name);
-        if ~exist(strcat(files_path_out{i},name,'.mat'), 'file')
-            video_file = [files_path_in{i}, name, '.avi'];
-            eye_position_file = [files_path_in{i}, name, '.txt'];
-            vid = VideoReader(video_file);
-            face_detector = vision.CascadeObjectDetector;
-            face_detector.MinSize = [400 400];
-            %face_detector.MaxSize = [800 800];
-            iframe = 0;
-            bbox = zeros(vid.FrameRate*vid.Duration, 4);
-
-            while hasFrame(vid)
-                iframe = iframe + 1;
-                frame = readFrame(vid);
-                temp = face_detector.step(frame);
-                if isempty(temp)
-                    bbox(iframe,:) = [0 0 0 0];
-                else
-                    bbox(iframe,:) = face_detector.step(frame);
-                end
-               % 
-            end
-            save(strcat(files_path_out{i},name),'bbox');
-        end
+        video_file = [files_path_in{i}, name, '.avi'];
+        eye_position_file = [files_path_in{i}, name, '.txt'];
+        Faces = Extract_faces(video_file, eye_position_file);
+            
     end
 end
