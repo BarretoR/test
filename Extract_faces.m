6d74892705f7e1474dd4aa459fe0c98ea2518962
chavesR <rogeriocbarreto@gmail.com>
2017-10-08 21:29:46 -0300

    Alterações nos arquivos Extract_faces.m e main.m

--- Extract_faces.m
+++ Extract_faces.m
@@ -1,28 +1,54 @@
 function Faces = Extract_faces(file_vid, file_eye_pos)
+    
     fileID = fopen(file_eye_pos);
     eye_pos = textscan(fileID, '%d %d %d %d %d', 'Delimiter', ',');
     vid = VideoReader(file_vid);
     face_detector = vision.CascadeObjectDetector;
-    face_detector.MinSize = [450 450];
+    face_detector.ScaleFactor = 1.01;
+    face_detector.MergeThreshold = 10;
     iframe = 0;
+    Faces.flag = 0;
     
     while hasFrame(vid)
         iframe = iframe + 1;
         frame = readFrame(vid);
         
         %Face Align
-        opposite = double(eye_pos{5}(iframe) - eye_pos{3}(iframe));
-        adjacent = double(eye_pos{4}(iframe) - eye_pos{2}(iframe));
-        angle_rotation = atand(opposite/adjacent);
-        frame = imrotate(frame, angle_rotation);
-        
-        temp = face_detector.step(frame);
-        if(~isempty(temp))
-            frame = imcrop(frame, temp);
-            Faces.data{iframe} = frame;
-            Faces.exist{iframe} = 1;
+        if (eye_pos{2}(iframe) ~= 0 && eye_pos{3}(iframe) ~= 0 && eye_pos{4}(iframe) ~= 0 && eye_pos{5}(iframe) ~= 0)
+            opposite = double(eye_pos{5}(iframe) - eye_pos{3}(iframe));
+            adjacent = double(eye_pos{4}(iframe) - eye_pos{2}(iframe));
+            angle_rotation = atand(opposite/adjacent);
+            frame = imrotate(frame, angle_rotation);
         end
         
+        if iframe == 1
+            
+            face_detector.MinSize = [400 400];
+            face_detector.MaxSize = [800 800];
+            temp = face_detector.step(frame);
+            
+            if ~isempty(temp)
+                
+                face_detector.MinSize = [temp(1,3)-70 temp(1,4)-70];
+                face_detector.MaxSize = [temp(1,3)+70 temp(1,4)+70];
+                
+            end
+        end
+        
+        temp = face_detector.step(frame);
+        
+        if(~isempty(temp))
+            
+            Faces.flag = Faces.flag + 1;
+            
+            frame = imresize(imcrop(frame, temp(1,:)), [100 100]);
+            Faces.data{iframe} = frame;
+            Faces.exist{iframe} = 1;
+        else
+            Faces.exist{iframe} = 0;
+        end
     end
-fclose(fileID);
+    
+    fclose(fileID);
+
 end
--- main.m
+++ main.m
@@ -5,15 +5,26 @@ clc
 files_path_in = {'/mnt/rogerio/Mestrado/FaceSpoofing/Datasets/OULU_NPU/Train_files/'}
 files_path_out = {'/mnt/rogerio/Mestrado/FaceSpoofing/Datasets/OULU_NPU/Train_inputs/'}
 %%
-parpool;
+parobj = parpool(2);
 for i = 1:numel(files_path_in)
     files = dir([files_path_in{i},'*.avi'])
     parfor j = 1:numel(files)
         j
         [pathstr, name, ext] = fileparts(files(j).name);
+        name
         video_file = [files_path_in{i}, name, '.avi'];
         eye_position_file = [files_path_in{i}, name, '.txt'];
-        Faces = Extract_faces(video_file, eye_position_file);
-            
+        if ~exist(strcat(files_path_out{i},name),'dir')
+            Faces = Extract_faces(video_file, eye_position_file);
+            if Faces.flag >= 50
+                mkdir(strcat('../../../../../..',files_path_out{i},name))
+                for a = 1:size(Faces.data,2)
+                    if Faces.exist{a} == 1
+                        imwrite(Faces.data{a},strcat('../../../../../../mnt/rogerio/Mestrado/FaceSpoofing/Datasets/OULU_NPU/Train_inputs/',name,'/',num2str(a),'.jpg'));
+                    end
+                end
+            end
+        end
     end
 end
+delete(parobj);
